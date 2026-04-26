ecc47ca9c4eb3720a2d7897f31d2d02c06d12382
Jehanzeb <jehanzebsiddiqui287@gmail.com>
2026-04-26 05:26:06 +0500

    enpoints fixed

--- lib/endpoints.ts
+++ lib/endpoints.ts
@@ -7,20 +7,20 @@ if (!api_url) {
 export const BASE_URL = api_url;
 
 export const ENDPOINTS = {
-  LOGIN: `/api/login`,
-  REGISTER: `/api/register`,
-  USER: `/api/user`,
+  LOGIN: `login`,
+  REGISTER: `register`,
+  USER: `user`,
 
   // Chat endpoints
-  CREATE_CHAT: `/api/chat/create-chat`,
-  GET_CHATS: `/api/chat/get-chats`,
-  GET_CHAT: (id: string) => `/api/chat/get-chat/${id}`,
-  DELETE_CHAT: (id: string) => `/api/chat/delete-chat/${id}`,
-  DELETE_ALL_CHATS: `/api/chat/delete-all-users-chat`,
+  CREATE_CHAT: `chat/create-chat`,
+  GET_CHATS: `chat/get-chats`,
+  GET_CHAT: (id: string) => `chat/get-chat/${id}`,
+  DELETE_CHAT: (id: string) => `chat/delete-chat/${id}`,
+  DELETE_ALL_CHATS: `chat/delete-all-users-chat`,
 
   // Message endpoints
-  SEND_MESSAGE: (chatId: string) => `/api/message/send-message/${chatId}`,
-  GET_MESSAGES: (chatId: string) => `/api/message/get-messages/${chatId}`,
-  DELETE_MESSAGE: (id: string) => `/api/message/delete-message/${id}`,
-  DELETE_ALL_MESSAGES: (chatId: string) => `/api/message/delete-all-messages/${chatId}`,
+  SEND_MESSAGE: (chatId: string) => `message/send-message/${chatId}`,
+  GET_MESSAGES: (chatId: string) => `message/get-messages/${chatId}`,
+  DELETE_MESSAGE: (id: string) => `message/delete-message/${id}`,
+  DELETE_ALL_MESSAGES: (chatId: string) => `message/delete-all-messages/${chatId}`,
 }
